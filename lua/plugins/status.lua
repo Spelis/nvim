96dba0c391435a4e833517f1784e7fee100b8566
Elis Eriksson <Spelis@duck.com>
2025-05-25 14:29:25 +0200

    added wakatime and neat little keypress listener i guess you can call it

--- lua/plugins/status.lua
+++ lua/plugins/status.lua
@@ -1,3 +1,38 @@
+local keypresses = {}
+
+vim.on_key(function(key)
+	local lookup = {
+		["<Space>"] = "󱁐",
+		["<CR>"] = "",
+		["<BS>"] = "󰁮",
+		["<Tab>"] = "",
+		["<Cmd>"] = "",
+		["<Esc>"] = "󱊷 ",
+		["<Up>"] = "k",
+		["<Down>"] = "j",
+		["<Left>"] = "h",
+		["<Right>"] = "l",
+	}
+
+	local k = vim.fn.keytrans(key)
+	if k:match("^<.+>$") and not lookup[k] then
+		return
+	end
+	k = lookup[k] or k
+
+	local last = keypresses[#keypresses]
+
+	if last and last.key == k then
+		last.count = last.count + 1
+	else
+		table.insert(keypresses, { key = k, count = 1 })
+	end
+
+	if #keypresses > 20 then
+		table.remove(keypresses, 1) -- pop oldest
+	end
+end)
+
 return {
 	{
 		"nvim-lualine/lualine.nvim",
@@ -35,7 +70,22 @@ return {
 						},
 					},
 				},
-				lualine_x = { "diagnostics" },
+				lualine_x = {
+					{
+						function()
+							local out = {}
+							for _, entry in ipairs(keypresses) do
+								if entry.count > 1 then
+									table.insert(out, tostring(entry.count) .. entry.key)
+								else
+									table.insert(out, entry.key)
+								end
+							end
+							return table.concat(out, "")
+						end,
+					},
+					"diagnostics",
+				},
 				lualine_y = { "location" },
 				lualine_z = { "filetype" },
 			},
